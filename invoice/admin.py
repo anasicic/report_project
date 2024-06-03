6f5b5fdc354d3238fd8b45ad7f9d687587bec95f
anasicic <121055104+anasicic@users.noreply.github.com>
2024-06-03 19:39:20 +0200

    Update admin.py

--- invoice/admin.py
+++ invoice/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Supplier, CostCenter
+from .models import Supplier, CostCenter, TypeOfCost
 
 admin.site.register(Supplier)
 admin.site.register(CostCenter)
+admin.site.register(TypeOfCost)
 
 # Register your models here.
